3217ebbf98d610fd0fd8c2927ee042ff841df69e
Sedra-za <119755030+Sedra-za@users.noreply.github.com>
2022-12-03 19:27:55 +0100

    Update Operatoren.java

--- src/Operatoren/Operatoren.java
+++ src/Operatoren/Operatoren.java
@@ -2,7 +2,7 @@ package Operatoren;
 
 public class Operatoren {
     public static void main(String[] args) {
-        // + - * / % < > = <= >= ++ -- += -= *= /* %= && || == !=
+        // + - * / % < > = <= >= ++ -- += -= *= /= %= && || == !=
         int x = 7;
         int y = 100;
         System.out.println(x);
